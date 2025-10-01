87c54036b1e98e754b907df39b65aed6c40cf1c7
uhejhjd <mc88880408@163.com>
2025-10-01 20:14:29 +0800

    更新内容

---
+++ App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Flurl;
+using Flurl.Http;
+using MinecraftLaunch;
+using MinecraftLaunch.Base.Enums;
+using MinecraftLaunch.Base.Models;
+using MinecraftLaunch.Base.Models.Game;
+using MinecraftLaunch.Base.Models.Network;
+using MinecraftLaunch.Components.Authenticator;
+using MinecraftLaunch.Components.Downloader;
+using MinecraftLaunch.Components.Installer;
+using MinecraftLaunch.Components.Logging;
+using MinecraftLaunch.Components.Parser;
+using MinecraftLaunch.Extensions;
+using MinecraftLaunch.Launch;
+using MinecraftLaunch.Utilities;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using Panuon.WPF.UI;
+
+namespace MCLaunch
+{
+
+    
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+    }
+
+}
